5a9ae894b9a5711bd854f00c7b5bc7ef7711a9e8
Iaquine Santos da Silva <61883817+Iaquine@users.noreply.github.com>
2024-07-19 23:15:46 -0300

    Update models.py
    Definicao do modelo para a tarefa (Task) no arquivo models.py do aplicativo todo

--- todoproject/todo/models.py
+++ todoproject/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Baixa', 'Baixa'),
+        ('Média', 'Média'),
+        ('Alta', 'Alta'),
+    ]
+
+    description = models.CharField(max_length=200)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+
+    def __str__(self):
+        return f"{self.description} - prioridade {self.priority}"
