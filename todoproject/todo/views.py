5abc1b1dd7eb411aadfb4acc804bbe5bb0d30b8f
Iaquine Santos da Silva <61883817+Iaquine@users.noreply.github.com>
2024-07-19 23:26:48 -0300

    Criacao do forms.py

--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/add_task.html', {'form': form})
